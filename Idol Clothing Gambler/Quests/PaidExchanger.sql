62a006bd84983be4370efb2fe004389610d75c31
Carthar <wade_grant@hotmail.com>
2025-01-04 14:21:22 -0500

    Add files via upload

---
+++ Idol Clothing Gambler/Quests/PaidExchanger.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'PaidExchanger';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('PaidExchanger', 9999, -1, 'Idol Quest Exchanger', '2025-1-04 00:00:00');
