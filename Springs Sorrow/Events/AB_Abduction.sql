72845db1dd82e78bd4cb8ac4e10e000450c9dd41
Carthar <wade_grant@hotmail.com>
2025-10-18 16:17:18 -0400

    Add files via upload

---
+++ Springs Sorrow/Events/AB_Abduction.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'AB_Abduction';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('AB_Abduction', -1, -1, 3, '2024-12-08 00:00:00');
---
+++ Springs Sorrow/Events/AB_Reward.sql
@@ -0,0 +1,4 @@
+DELETE FROM `event` WHERE `name` = 'AB_Reward';
+
+INSERT INTO `event` (`name`, `start_Time`, `end_Time`, `state`, `last_Modified`)
+VALUES ('AB_Reward', -1, -1, 3, '2024-12-22 00:00:00');
