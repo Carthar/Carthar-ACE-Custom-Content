e5cf11b1bca448bd19e1ab8b4f8cb1eb00e79cbd
Carthar <wade_grant@hotmail.com>
2025-10-18 16:17:46 -0400

    Add files via upload

---
+++ Springs Sorrow/Quests/TornAdirredShroud.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE `name` = 'TornAdirredShroud';
+
+INSERT INTO `quest` (`name`, `min_Delta`, `max_Solves`, `message`, `last_Modified`)
+VALUES ('TornAdirredShroud', 432000, -1, 'Cooldown for Panopticon Reward', '2024-12-13 00:00:00');
