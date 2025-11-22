44c0e42cfebbd7d19db23b7cc4271b09559acba5
Carthar <wade_grant@hotmail.com>
2025-11-21 21:41:47 -0500

    Update 261006 Large Heavy Ball of Tusker Poop.sql

--- Springs Sorrow/Weenies/261006 Large Heavy Ball of Tusker Poop.sql
+++ Springs Sorrow/Weenies/261006 Large Heavy Ball of Tusker Poop.sql
@@ -5,7 +5,7 @@ VALUES (261006, '261006 Large Heavy Ball of Tusker Poop', 1, '2024-12-21 08:00:0
 
 INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
 VALUES (261006,   1,        128) /* ItemType - Misc */
-     , (261006,   5,         35) /* EncumbranceVal */
+     , (261006,   5,      84000) /* EncumbranceVal */
      , (261006,   8,      73500) /* Mass */
      , (261006,   9,   16777216) /* ValidLocations - Held */
      , (261006,  16,          1) /* ItemUseable - No */
@@ -34,4 +34,5 @@ SET @parent_id = LAST_INSERT_ID();
 INSERT INTO `weenie_properties_emote_action` (`emote_Id`, `order`, `type`, `delay`, `extent`, `motion`, `message`, `test_String`, `min`, `max`, `min_64`, `max_64`, `min_Dbl`, `max_Dbl`, `stat`, `display`, `amount`, `amount_64`, `hero_X_P_64`, `percent`, `spell_Id`, `wealth_Rating`, `treasure_Class`, `treasure_Type`, `p_Script`, `sound`, `destination_Type`, `weenie_Class_Id`, `stack_Size`, `palette`, `shade`, `try_To_Bond`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
 VALUES (@parent_id, 0, 1 /* Act */, 0, 1, NULL, 'The ball of poop drops to the floor with a squishy plonk sound.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
      , (@parent_id, 1, 88 /* LocalSignal */, 0, 1, NULL, 'PoopDrop', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
 
